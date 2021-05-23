17f40a9b39c7cddd1502133517dc9cbc0a8e5a0b
Maddy B <maddx@live.com.au>
2021-05-23 17:44:09 +0800

    Create an Order Class then iterate through each menu item and add the item to a new order.

--- DinerMax3000Console/Program.cs
+++ DinerMax3000Console/Program.cs
@@ -14,6 +14,17 @@
             outsideDrinks.Disclaimer = "Do not drink and code.";
             outsideDrinks.AddMenuItem("Wine O", "red delicious", 10);
             outsideDrinks.AddMenuItem("Its Yellow?", "Salty and Sweet", 10);
+
+            Order hungryGuestOrder = new Order();
+            for (int x=0; x<=summerMenu.items.Count-1; x++)
+            {
+                MenuItem currentItem = summerMenu.items[x];
+                hungryGuestOrder.items.Add(currentItem);
+            }
+            foreach(MenuItem currentItem in outsideDrinks.items)
+            {
+                hungryGuestOrder.items.Add(currentItem);
+            }
         }
     }
 }
