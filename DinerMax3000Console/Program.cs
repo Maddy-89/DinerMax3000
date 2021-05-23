9a7e8d3e996530a572dcc345babef6a0c2419777
Maddy B <maddx@live.com.au>
2021-05-23 17:04:34 +0800

    Add MenuItem and Menu Classes. Create a hard coded menu item and add it to the menu.

--- DinerMax3000Console/Program.cs
+++ DinerMax3000Console/Program.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DinerMax3000Console
+﻿namespace DinerMax3000Console
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Menu summerMenu = new Menu();
+            summerMenu.Name = "Summer menu";
+            MenuItem salmon = new MenuItem();
+            salmon.Title = "Salmon";
+            salmon.Description = "Fresh Fish";
+            salmon.Price = 25.50;
+            summerMenu.items.Add(salmon);
         }
     }
 }
