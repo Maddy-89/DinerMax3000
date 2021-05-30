75520197f89807ccdf730df2a44740baae6080c8
Maddy B <maddx@live.com.au>
2021-05-30 13:49:21 +0800

    Connect Database, Add dataset and table adapters

--- DinerMax3000BusinessLayer/Menu.cs
+++ DinerMax3000BusinessLayer/Menu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DinerMax3000.Business.dsDinerMax3000TableAdapters;
 
 namespace DinerMax3000.Business
 {
@@ -12,7 +13,34 @@ namespace DinerMax3000.Business
         {
             items = new List<MenuItem>();
         }
+        private int _databaseId;
 
+        public void SaveNewMenuItem(string Name, string Description, double Price)
+        {
+            MenuItemTableAdapter tamenuItem = new MenuItemTableAdapter();
+            tamenuItem.InsertNewMenuItem(Name, Description, Price, _databaseId);
+        }
+        public static List<Menu> GetAllMenus()
+        {
+            MenuTableAdapter taMenu = new MenuTableAdapter();
+            MenuItemTableAdapter taMenuItem = new MenuItemTableAdapter();
+            var dtMenu = taMenu.GetData();
+            List<Menu> allMenus = new List<Menu>();
+            foreach(dsDinerMax3000.MenuRow menuRow in dtMenu.Rows)
+            {
+                Menu currentMenu = new Menu();
+                currentMenu.Name = menuRow.Name;
+                currentMenu._databaseId = menuRow.Id;
+                allMenus.Add(currentMenu);
+
+                var dtMenuItems = taMenuItem.GetMenuItemsByMenuId(menuRow.Id);
+                foreach(dsDinerMax3000.MenuItemRow menuItemRow in dtMenuItems.Rows)
+                {
+                    currentMenu.AddMenuItem(menuItemRow.Name, menuItemRow.Description, menuItemRow.Price);
+                }
+            }
+            return allMenus;
+        }
         public void AddMenuItem(string Title, string Description, double Price)
         {
             MenuItem item = new MenuItem();
--- DinerMax3000Console/Program.cs
+++ DinerMax3000Console/Program.cs
@@ -11,38 +11,19 @@ namespace DinerMax3000Console
     {
         static void Main(string[] args)
         {
-            FoodMenu summerMenu = new FoodMenu();
-            summerMenu.Name = "Summer menu";
-            summerMenu.AddMenuItem("Salmon", "Garlic Butter Fresh Salmon", 25.50);
-            summerMenu.AddMenuItem("Taco", "Salty Chips", 10);
-            summerMenu.HospitalDirections = "Right around the corner!";
-
-            DrinkMenu outsideDrinks = new DrinkMenu();
-            outsideDrinks.Disclaimer = "Do not drink and code.";
-            outsideDrinks.AddMenuItem("Wine O", "red delicious", 10);
-            outsideDrinks.AddMenuItem("Its Yellow?", "Salty and Sweet", 10);
-
+            List<Menu> menusFromDatabase = Menu.GetAllMenus();
+            Menu firstMenu = menusFromDatabase[0];
+            firstMenu.SaveNewMenuItem("Smorgas", "A classic nordic dish.", 10);
+            menusFromDatabase = Menu.GetAllMenus();
             Order hungryGuestOrder = new Order();
-            for (int x=0; x<=summerMenu.items.Count-1; x++)
+            foreach(Menu CurrentMenu in menusFromDatabase)
             {
-                MenuItem currentItem = summerMenu.items[x];
-                hungryGuestOrder.items.Add(currentItem);
+                foreach (MenuItem currentItem in CurrentMenu.items)
+                {
+                    hungryGuestOrder.items.Add(currentItem);
+                }
             }
-            foreach(MenuItem currentItem in outsideDrinks.items)
-            {
-                hungryGuestOrder.items.Add(currentItem);
-            }
-
             Console.WriteLine("The total is:" + hungryGuestOrder.Total);
-
-            try
-            {
-                outsideDrinks.AddMenuItem("Free Me?", "You get nothing", 0);
-            }
-            catch (Exception thrownException)
-            {
-                Console.WriteLine(value: thrownException.Message);
-            }
             Console.ReadKey();
         }
     }
