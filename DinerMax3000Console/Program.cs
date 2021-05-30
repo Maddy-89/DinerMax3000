252336a6e18c85a1dd21c8ed563bb6515fd17e8d
Maddy B <maddx@live.com.au>
2021-05-30 09:28:15 +0800

    Add a new project to separate business layer (logic), Move Classes and add references to new project.

--- DinerMax3000Console/Program.cs
+++ DinerMax3000Console/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DinerMax3000.Business;
 
 namespace DinerMax3000Console
 {
